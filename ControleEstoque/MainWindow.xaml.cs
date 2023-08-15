bf771d54e14dc6fd5977cb47962237620919df91
Bruna Mattioli <bru.mattioli@gmail.com>
2023-08-15 12:19:19 -0300

    Implementação do botão cadastrar, filtrar e interface inicial para primeira entrega.

---
+++ ControleEstoque/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using ControleEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ControleEstoque
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DalHelper.CriarBancoSQLite();
+            DalHelper.CriarTabelaSQlite();
+        }
+
+        private void Cadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Valida())
+            {
+                MessageBox.Show("Informe os dados do produto a incluir");
+                return;
+            }
+            try
+            {
+                MessageBoxResult dr = MessageBox.Show("Deseja cadastrar as informações abaixo?" + "\n\r" + "Código: " + codigo.Text + "\n\r" + "Nome: " + nome.Text + "\n\r" + "Preço: " + preco.Text + "\n\r" + "Quantidade: " + quantidade.Text, "Confirmação", MessageBoxButton.OKCancel);
+                switch (dr)
+                {
+                    case MessageBoxResult.OK:
+                        Produto product = new Produto();
+                        product.Id = Convert.ToInt32(codigo.Text);
+                        product.Nome = nome.Text;
+                        product.Preco = Convert.ToDouble(preco.Text);
+                        product.Quantidade = Convert.ToInt32(quantidade.Text);
+                        DalHelper.Add(product);
+                        MessageBox.Show("Produto cadastrado com sucesso!");
+                        emptyBoxesReg();
+                        break;
+                    case MessageBoxResult.Cancel:
+                        MessageBox.Show("Produto não cadastrado, insira as informações novamente.");
+                        emptyBoxesReg();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+
+        }
+        private void btnExibirDados_Click(object sender, EventArgs e)
+        {
+            ExibirDados();
+        }
+
+        private void ExibirDados()
+        {
+            try
+            {
+                List<Produto> l = DalHelper.GetProdutos();
+                stockView.ItemsSource = l;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+        }
+
+        private bool Valida()
+        {
+            if (string.IsNullOrEmpty(nome.Text) && string.IsNullOrEmpty(codigo.Text) && string.IsNullOrEmpty(preco.Text) && string.IsNullOrEmpty(quantidade.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void nome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void codigo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void preco_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void quantidade_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        //LIMPAR CAMPOS DE CADASTRAR PRODUTO
+        private void emptyBoxesReg()
+        {
+            try
+            {
+                nome.Text = "";
+                codigo.Text = "";
+                preco.Text = "";
+                quantidade.Text = "";
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show(f.Message.ToString());
+            }
+        }
+
+        private void noFilter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                nomeBusca.Text = "";
+                codigoBusca.Text = "";
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show(f.Message.ToString());
+            }
+        }
+
+        private void filterButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(codigoBusca.Text) && string.IsNullOrEmpty(nomeBusca.Text))
+            {
+                MessageBox.Show("Informe o código ou o nome do produto a ser Localizado");
+                return;
+            }
+            try
+            {
+                if (string.IsNullOrEmpty(nomeBusca.Text))
+                {
+                    int Id = Convert.ToInt32(codigoBusca.Text);
+                    List<Produto> l = DalHelper.GetProduto_Id(Id);
+                    stockView.ItemsSource = l;
+                }
+                else
+                {
+                    string Nome = nomeBusca.Text;
+                    List<Produto> l = DalHelper.GetProduto_Nome(Nome);
+                    stockView.ItemsSource = l;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+        }
+
+        private void stockView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void codigoBusca_TextChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void nomeBusca_TextChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ControleEstoque/Models/DalHelper.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Documents;
+
+namespace ControleEstoque.Models
+{
+    public class DalHelper
+    {
+        private static SQLiteConnection sqliteConnection;
+
+        public DalHelper()
+        { }
+
+        private static SQLiteConnection DbConnection()
+        {
+            string baseConnectionString = "Data Source=db_controle_estoque";
+            var connectionString = new SqliteConnectionStringBuilder(baseConnectionString)
+            {
+                Mode = SqliteOpenMode.ReadWriteCreate,
+            }.ToString();
+
+            sqliteConnection = new SQLiteConnection(connectionString);
+            sqliteConnection.Open();
+            return sqliteConnection;
+        }
+
+        public static void CriarBancoSQLite()
+        {
+            try
+            {
+                string dbPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                "db_controle_estoque");
+                bool exists = File.Exists(dbPath);
+                if (!exists)
+                    SQLiteConnection.CreateFile(dbPath);
+                //SQLiteConnection.CreateFile(@"C:\Users\bruma\Downloads\db_controle_estoque");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static void CriarTabelaSQlite()
+        {
+            try
+            {
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS Produtos(Id int, Nome Varchar(50), Preco double, Quantidade int)";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void Add(Produto product)
+        {
+            try
+            {
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO Produtos(Id, Nome, Preco, Quantidade) values (@Id, @Nome, @Preco, @Quantidade)";
+                    cmd.Parameters.AddWithValue("@Id", product.Id);
+                    cmd.Parameters.AddWithValue("@Nome", product.Nome);
+                    cmd.Parameters.AddWithValue("@Preco", product.Preco);
+                    cmd.Parameters.AddWithValue("@Quantidade", product.Quantidade);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        public static List<Produto> GetProdutos()
+        {
+            //SQLiteDataAdapter da = null;
+            //DataSet ds = new DataSet();
+            try
+            {
+                List<Produto> p = new List<Produto>();
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Produtos";
+                    using SQLiteDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Produto op = new Produto();
+                        op.Id = rdr.GetInt32(0);
+                        op.Nome = rdr.GetString(1);
+                        op.Preco = rdr.GetDouble(2);
+                        op.Quantidade = rdr.GetInt32(3);
+                        p.Add(op);
+                    }
+                    return p;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static List<Produto> GetProduto_Nome(string Nome)
+        {
+            try
+            {
+                List<Produto> p = new List<Produto>();
+                using (var cmd = DbConnection().CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Produtos Where Nome ='"+ Nome +"'";
+                    using SQLiteDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Produto op = new Produto();
+                        op.Id = rdr.GetInt32(0);
+                        op.Nome = rdr.GetString(1);
+                        op.Preco = rdr.GetDouble(2);
+                        op.Quantidade = rdr.GetInt32(3);
+                        p.Add(op);
+                    }
+                    return p;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static List<Produto> GetProduto_Id(int Id)
+        {
+            try
+            {
+            List<Produto> p = new List<Produto>();
+            using (var cmd = DbConnection().CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Produtos Where Id =" + Id;
+                using SQLiteDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Produto op = new Produto();
+                    op.Id = rdr.GetInt32(0);
+                    op.Nome = rdr.GetString(1);
+                    op.Preco = rdr.GetDouble(2);
+                    op.Quantidade = rdr.GetInt32(3);
+                    p.Add(op);
+                }
+                return p;
+            }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
